8fcf28fe20d307b24e8468d6a69a11078eb7a196
Alexander Hurd <hurdad@gmail.com>
2016-04-04 01:39:01 -0400

    update example

--- examples/ha_pool.cpp
+++ examples/ha_pool.cpp
@@ -5,7 +5,7 @@ using namespace redis3m;
 
 int main(int argc, char **argv)
 {
-    connection_pool::ptr_t pool = connection_pool::create("<yoursentinel>", "test", 500000); //500ms timeout
+    connection_pool::ptr_t pool = connection_pool::createTimeout("sentinel1,sentinel2,sentinel3", "test", 26379, 1, 500000); //1 sec + 500000 usec [1.5 sec] timeout
     
     connection::ptr_t c = pool->get(connection::MASTER);
     c->run(command("SET") << "foo" << "bar");
